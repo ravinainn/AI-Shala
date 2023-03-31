535cabe17e0cda2af8a7b31937b755ab3ad4f09c
Ravi Nain <91778452+ravinainn@users.noreply.github.com>
2023-03-31 19:30:13 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,16 @@ const  toggleNav = () => {
 }
 
 mobile_nav.addEventListener('click', () => toggleNav() );
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
